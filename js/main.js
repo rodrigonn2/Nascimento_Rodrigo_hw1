8991c9b5859d5b33c8c774a59e7e30f919a8966c
rodrigonn2 <144962992+rodrigonn2@users.noreply.github.com>
2024-01-28 01:11:18 -0500

    updating html and svg
    updating html code to make the strtucture and the first animation

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//The Window.console property returns a reference to the console object, which provides methods for logging information to the browser's console.
+console.log("JS file connected");
+
+const vectorGraphic = document.querySelector('#water_img');
+
+function logThisId() {
+    console.log(this);
+    console.log('clicked on this element', this.id);
+
+}
+
+vectorGraphic.addEventListener('click', logThisId);
